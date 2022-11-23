f211c00c29a9a82bdb9cbfdc3e3f5844803b54d4
Kris <kharonek@yahoo.com>
2022-11-23 00:39:31 +0000

    minor drawing fix

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local rules = {}
 
 rules[1] = {
     ['a'] = "F",
-    ['b'] = 'F[+F]F'
+    ['b'] = 'FF+[+F-F-F]-[-F+F+F]'
 }
 -- FF+[+F-F-F]-[-F+F+F]
 
@@ -40,7 +40,7 @@ function generate()
     end
 
     sentence = nextSentence
-    print(sentence)
+   -- print(sentence)
 
 end
 
@@ -49,9 +49,10 @@ lHeight = 10
 
 -- next x, y
 nx = tx
-ny = ty-lHeight
+ny = ty - lHeight
 
-a = 0.7
+a = 0
+theta = 0.7
 -- table.remove(tableName, index) to pop index = #tableName
 -- table[#tableName+1] = {x,y} to push
 
@@ -60,45 +61,96 @@ a = 0.7
 simpleStack = {}
 simpleStack[1] = {200, 250}
 
-
 function turtle()
-  for i=1, #sentence do
-    local current = sentence:sub(i, i)
-    
-    if current == "F" then
-        display.newLine(tx, ty, nx, ny)
-        tx = nx
-        ty = ny
-        print(tx, ty)
+    for i = 1, #sentence do
+        local current = sentence:sub(i, i)
 
-    elseif current == "+" then
-      -- rotate right
-      --nl:rotate(45)
-      dx = lHeight * math.cos(a)
-      dy = lHeight * math.sin(a)
+        if current == "F" then
+            display.newLine(tx, ty, nx, ny)
 
-      nx = nx + dx
-      ny = ny - dy
-     -- display.newLine(tx, ty, tx+dx, ty-dy)
+            tx = nx
+            ty = ny
 
-    elseif current == "[" then
-        -- push
-        simpleStack[#simpleStack+1] = {tx, ty, nx, ny}
-        print("position saved")
-        print(simpleStack[#simpleStack][1], simpleStack[#simpleStack][2])
+            if a == 0 then
+                ny = ty - lHeight
+            end
 
-    elseif  current == "]" then
-        -- pop
-        tx = simpleStack[#simpleStack][1]
-        ty = simpleStack[#simpleStack][2]
-        nx = simpleStack[#simpleStack][3]
-        ny = simpleStack[#simpleStack][4]
+        elseif current == "+" then
+            -- rotate right
+            -- nl:rotate(45)
+            -- polar cooridnates key???
+            a = a + theta
+            dx = lHeight * math.cos(a)
+            dy = lHeight * math.sin(a)
 
-        print("poped position:")
-        print(tx, ty, nx, ny)
+            nx = nx + dx
+            ny = ny - dy
+            -- display.newLine(tx, ty, tx+dx, ty-dy)
 
-    elseif current == "-" then
-        -- rotate left
+        elseif current == "[" then
+            -- push
+            -- when saving there is no nx because turtle moved there already
+            simpleStack[#simpleStack + 1] = {tx, ty, a}
+
+        elseif current == "]" then
+            -- pop
+            tx = simpleStack[#simpleStack][1]
+            ty = simpleStack[#simpleStack][2]
+            a = simpleStack[#simpleStack][3]
+
+        elseif current == "-" then
+            -- rotate left
+            a = a + theta
+            dx = lHeight * math.cos(a)
+            dy = lHeight * math.sin(a)
+
+            nx = nx - dx
+            ny = ny - dy
+        end
+        print(a)
     end
-  end
+
+    print(sentence)
+
 end
+
+function turtle2()
+    local height = 10
+    local x, y = 200, 250
+    local nx, ny = x, y - height -- next x, y
+    local angle = 0
+    local dAngle = 45 -- delta angle
+    local sx, sy, sa = x, y, angle -- save variables
+
+    for i = 1, #sentence do
+        local current = sentence:sub(i, i)
+
+        if current == "F" then
+            local dx = height * math.cos(dAngle)
+            local dy = height * math.sin(dAngle)
+            print(dx, dy)
+            nx = nx + dx
+            ny = ny - dy
+
+            -- draw line x,y to nx, ny
+            display.newLine(x, y, nx, ny)
+
+        elseif current == "+" then
+            -- rotate right
+            angle = angle + dAngle
+
+        elseif current == "[" then
+            -- push
+            local sx, sy, sa = x, y, angle
+
+        elseif current == "]" then
+            -- pop
+            x, y, angle = sx, sy, sa
+
+        elseif current == "-" then
+            -- rotate left
+            angle = angle - dAngle
+        end
+    end
+end
+
